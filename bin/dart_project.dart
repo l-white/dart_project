0b5d581b344f87a98d16baf61d78706f4654028c
Laura <law7676@outlook.com>
2021-06-07 06:40:12 -0500

    started game logic

--- bin/dart_project.dart
+++ bin/dart_project.dart
@@ -1,4 +1,27 @@
-void main(List<String> arguments) {
-  String name = 'Laura';
-  print('Hello ${name}');
+import 'dart:io';
+void main() {
+  while (true){
+    stdout.write('Rock, paper or scissors? (r/p/s');
+    final input = stdin.readLineSync();
+    if (input == 'r' || input == 's' || input == 's'){
+      print('You selected $input');
+    } else if (input == 'q'){
+      break;
+    } else {
+      print('Invalid input');
+    }
+  }
+  // While true
+
+  // Show prompt
+
+  // Read user input from console
+
+  // Check if user input is a valid move "r", "p" or "s"
+
+    // If yes, choose the AI move at random
+
+    // Compare player move with the AI move
+
+    // Show result
 }
