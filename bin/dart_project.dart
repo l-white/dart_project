a354371d29f8b93d99b2bd9ef4c4de06bfa6eee2
Laura <law7676@outlook.com>
2021-06-07 06:58:21 -0500

    Added additional game logic

--- bin/dart_project.dart
+++ bin/dart_project.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
+import 'dart:math';
 
 enum Move { rock, paper, scissors }
 void main() {
+  final rng = Random();
   while (true){
     stdout.write('Rock, paper or scissors? (r/p/s');
     final input = stdin.readLineSync();
@@ -13,6 +15,17 @@ void main() {
         playerMove = Move.paper;
       } else {
         playerMove = Move.scissors;
+      }
+      final random = rng.nextInt(3);
+      final aiMove = Move.values[random];
+      if (playerMove == aiMove){
+        print("It's a draw");
+      } else if (playerMove == Move.rock && aiMove == Move.scissors ||
+        playerMove == Move.paper && aiMove == Move.rock ||
+        playerMove == Move.scissors && aiMove == Move.paper){
+        print('You win!');
+      } else {
+        print('Computer wins.');
       }
       print('You selected $input');
     } else if (input == 'q'){
